6f08b8eb2346a178208d34b66efd460dc2024075
ZACHFREITAS@hotmail.com <ZACHFREITAS@hotmail.com>
2022-02-05 21:22:01 +0000

    save

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -8,6 +8,6 @@ with payments as (
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
-    from {{ source('jaffle_shop', 'payments') }}
+    from {{ source('jaffle_shop', 'payment') }}
 )
 select * from payments
